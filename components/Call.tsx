8600500e3b307a54199952df95d41c433f075091
Lalit Max <lalitmay@gmail.com>
2025-01-04 00:34:35 +0530

    fix a tag to Link for nextjs eslint

--- components/Call.tsx
+++ components/Call.tsx
@@ -12,6 +12,7 @@ import AgoraRTC, {
   useRemoteAudioTracks,
   useRemoteUsers,
 } from "agora-rtc-react";
+import Link from "next/link";
 
 function Call(props: { appId: string; channelName: string }) {
   const client = useRTCClient(
@@ -22,12 +23,12 @@ function Call(props: { appId: string; channelName: string }) {
     <AgoraRTCProvider client={client}>
       <Videos channelName={props.channelName} AppID={props.appId} />
       <div className="fixed z-10 bottom-0 left-0 right-0 flex justify-center pb-4">
-        <a
+        <Link
           className="px-5 py-3 text-base font-medium text-center text-white bg-red-400 rounded-lg hover:bg-red-500 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 w-40"
           href="/"
         >
           End Call
-        </a>
+        </Link>
       </div>
     </AgoraRTCProvider>
   );
@@ -93,10 +94,10 @@ function Videos(props: { channelName: string; AppID: string }) {
             remoteUsers.length > 9
               ? unit.repeat(4)
               : remoteUsers.length > 4
-              ? unit.repeat(3)
-              : remoteUsers.length > 1
-              ? unit.repeat(2)
-              : unit,
+                ? unit.repeat(3)
+                : remoteUsers.length > 1
+                  ? unit.repeat(2)
+                  : unit,
         }}
       >
         {/* Local video track */}
