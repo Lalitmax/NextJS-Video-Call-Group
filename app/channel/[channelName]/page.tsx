987cbd55153a9ce440892d186612c847a26bb506
Lalit Max <lalitmay@gmail.com>
2025-01-04 01:04:24 +0530

    fixed params how to take

--- app/channel/[channelName]/page.tsx
+++ app/channel/[channelName]/page.tsx
@@ -1,24 +1,20 @@
 import { FC } from "react";
+import { useParams } from "next/navigation";
 import Call from "@/components/Call";
 
- interface PageProps {
-  params: {
-    channelName: string;
-  };
-}
+const Page: FC = () => {
+    const params = useParams();
+    const channelName = params.channelName as string;
 
-const Page: FC<PageProps> = ({ params }) => {
-  return (
-    <main className="flex w-full flex-col">
-      {/* Displaying the channel name */}
-      <p className="absolute z-10 mt-2 ml-12 text-2xl font-bold text-gray-900">
-        Channel: {params.channelName}
-      </p>
-      
-      {/* Call component for video call */}
-      <Call appId={process.env.PUBLIC_AGORA_APP_ID!} channelName={params.channelName} />
-    </main>
-  );
+    return (
+        <main className="flex w-full flex-col">
+            <p className="absolute z-10 mt-2 ml-12 text-2xl font-bold text-gray-900">
+                Channel: {channelName}
+            </p>
+
+            <Call appId={process.env.PUBLIC_AGORA_APP_ID!} channelName={channelName} />
+        </main>
+    );
 };
 
 export default Page;
