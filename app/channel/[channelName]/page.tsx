441df25384757f74ae85bde0be47ce1d48a58dbd
Lalit Max <lalitmay@gmail.com>
2025-01-04 00:42:13 +0530

    fixed nextjs type in channel props page

--- app/channel/[channelName]/page.tsx
+++ app/channel/[channelName]/page.tsx
@@ -1,12 +1,20 @@
+import { FC } from "react";
 import Call from "@/components/Call";
 
-export default function Page({ params }: { params: { channelName: string } }) {
+interface PageProps {
+    params: {
+        channelName: string;
+    };
+}
+
+export const Page: FC<PageProps> = ({ params }) => {
     return (
         <main className="flex w-full flex-col">
             <p className="absolute z-10 mt-2 ml-12 text-2xl font-bold text-gray-900">
-                {params.channelName!}
+                {params.channelName}
             </p>
-            <Call appId={process.env.PUBLIC_AGORA_APP_ID!} channelName={params.channelName}></Call>
+            <Call appId={process.env.PUBLIC_AGORA_APP_ID!} channelName={params.channelName} />
         </main>
-    )
-}
+    );
+};
+
